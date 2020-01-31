5c0b494a1a0f544ddd69a2f51b2a86635da21f3a
MrVizious <mrvizious@gmail.com>
2020-02-01 00:26:51 +0100

    Basic Movement Implemented

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    private Transform t;
+    // Start is called before the first frame update
+    void Start()
+    {
+        t = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 goal = new Vector2(t.position.x + Input.GetAxis("Horizontal") * Time.deltaTime * speed, t.position.y + Input.GetAxis("Vertical") * Time.deltaTime * speed);
+        t.position = Vector2.MoveTowards(t.position, goal, speed);
+
+        float rot_z = Mathf.Atan2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * Mathf.Rad2Deg;
+        t.rotation = Quaternion.Euler(0f, 0f, rot_z - 180);
+    }
+}
