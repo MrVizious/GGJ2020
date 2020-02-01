71d51b728d49801bfb63ed9c8a2a8f61ebd793c4
VBL99 <52764754+VBL99@users.noreply.github.com>
2020-02-01 19:50:46 +0100

    Base Menu
    Esta la base del menu de pausa, no hay codigo alguno. Hay un prefab del menu de pausa

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour {
+
+    public GameObject Jugar, Salirs;
+    public bool ON, Mute;
+   
+
+
+    public void aPlay()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    public void aExit()
+    {
+        Application.Quit();
+    }   
+}
