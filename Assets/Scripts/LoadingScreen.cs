f80785a63d9ec078419f3bb20955d6d222252651
VBL99 <52764754+VBL99@users.noreply.github.com>
2020-02-01 17:50:15 +0100

    Pantalla de carga
    Pantalla de carga implementada
    Nota: Carga muy rapido y hay que en cada escena hay que ponerlo en un objeto vacio
    Modificarlo para nuestro juego (recomendacion: cuando llegue a x enemigos muertos por puntuacion, que se active)

---
+++ Assets/Scripts/LoadingScreen.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadingScreen : MonoBehaviour
+{
+    public string levelToLoad;
+
+    public GameObject loadscreen;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if  (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            loadscreen.SetActive(true);
+            //SceneManager.Load(levelToLoad)
+            StartCoroutine(LoadLevelAsync());
+
+        }
+    }
+
+    private IEnumerator LoadLevelAsync()
+    {
+        AsyncOperation asyncLoad = SceneManager.LoadSceneAsync(levelToLoad);
+
+        while(!asyncLoad.isDone)
+        {
+            yield return null;
+        }
+
+
+    }
+}
