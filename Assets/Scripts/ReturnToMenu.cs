6ae12e349e3bcc3e269af95b5ca95032021f6638
VBL99 <52764754+VBL99@users.noreply.github.com>
2020-02-02 12:02:29 +0100

    Muerte
    Escena de muerte implementada

---
+++ Assets/Scripts/ReturnToMenu.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ReturnToMenu : MonoBehaviour
+{
+    
+    void Update()
+    {
+        if (Input.GetKeyDown("joystick button 9"))
+        {
+            SceneManager.LoadScene("Menu");
+             print ("it just works");
+        }
+       
+       
+    }
+}
